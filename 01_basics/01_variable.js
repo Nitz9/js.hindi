82737320ff50fa4168d73bb250fc67f9d31c9d2c
Nitz9 <68243954+Nitz9@users.noreply.github.com>
2024-07-20 17:54:18 +0000

    variable and datatypes done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 14253
+let accountEmail = "sam@gmail.com"
+var accountPassword= "123456"
+accountCity = "Delhi"
+
+
+// accountId = 2  not changeable because "const"
+// accountEmail = "Nitz@gmail.com"  changeable due to "let"  --- only use Let to create a variable
+
+// accountPassword = "111111"     changeable due to "var"  (its scope is global)
+
+// accountCity = "Mumbai"   changeable due to when a variable is declared without any prefix
+
+console.log(accountCity);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict" // treat all JS code as newer version
+
+// alert("Hello")     we are using Node.js so we can't use "alert"
+
+/* 
+number 
+bigInt
+string
+boolean
+null
+undefined
+symbol
+*/
