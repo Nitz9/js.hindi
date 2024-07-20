2a3f6c436ed5b4aa3978e6db5bafd7e2c56f51bb
Nitz9 <68243954+Nitz9@users.noreply.github.com>
2024-07-20 18:09:40 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,28 @@
+// let score = "33"
+// let score2 = "22@abc"
+// let score3 = undefined
+
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// let value2 = Number (score2)
+// console.log(typeof valueInNumber)
+// console.log(typeof value2)
+// console.log(value2)
+// console.log(typeof score3)
+// console.log(score3)
+
+let isLoggedIn1 = 1
+let isLoggedIn2 = ""
+let isLoggedIn3 = "nitz"
+
+let login1 =  Boolean(isLoggedIn1)
+let login2 = Boolean (isLoggedIn2)
+let login3 = Boolean (isLoggedIn3)
+
+console.log(login1)
+console.log(login2)
+console.log(login3)
